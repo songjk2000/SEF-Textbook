a9fe06592b646e9df250451bd731461b7d2d9866
Leonel Song <45224952+songjk2000@users.noreply.github.com>
2020-02-26 20:35:35 +0800

    Update classic.php

--- theme/classic.php
+++ theme/classic.php
@@ -18,7 +18,7 @@
         .changelanguage{position:absolute;right:5px;}
         .title{text-align:center;height:10%;letter-spacing:2px;}
         .title a{color:#FFFFFF;text-decoration:none}
-        .list-wrapper{height:80%;width:80%;margin:0 auto;position:relative;border-radius:15px;}
+        .list-wrapper{min-height:80%;width:80%;margin:0 auto;position:relative;border-radius:15px;}
         .list-container{height:100%;background-color:#FFFFFF;position:relative;overflow:hidden;border-radius:15px;}
         .list-header-container{position:relative}
         .list-header-container a.back-link{color:#000;display:inline-block;position:absolute;font-size:16px;margin:20px 10px;padding:10px 10px;vertical-align:middle;text-decoration:none}
